42305c0cc3bf91d0728fd9880310fc0db1469d2b
loganmw3 <loganmw3@illinois.edu>
2026-02-14 10:23:43 -0600

    updated example tb w masks

--- template/sim/test_example.py
+++ template/sim/test_example.py
@@ -2,17 +2,24 @@ import cocotb
 from cocotb.triggers import Timer
 import random
 
+MASK8 = 0xFF
+MASK16 = 0xFFFF
+MASK32 = 0xFFFFFFFF
+MASK64 = 0xFFFFFFFFFFFFFFFF
+
 @cocotb.test()
 async def test_example(dut):
     random.seed(0) # Used to replicate results
     
-    a_vals = tuple(random.randint(0, 1) for _ in range(100))
     
-    for a in a_vals:
+    for _ in range(100):
+        a = random.randint(0,1)
         dut.a.value = a
         
         await Timer(1, unit="ns")
-        assert int(dut.y.value) == a, f"ERROR: Expected: {a}, Got: {dut.y.value}"
+        expected = a
+        got = int(dut.y.value)
+        assert expected == got, f"ERROR: Expected: {expected}, Got: {got}"
 
     
     await Timer(10, unit="ns")
